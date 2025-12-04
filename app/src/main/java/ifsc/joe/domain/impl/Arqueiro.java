b827b32e770827b14b6bf47d55c3bc64a1455dfe
gelson-jr <gelson.j@aluno.ifsc.edu.br>
2025-12-04 17:17:32 -0300

    Adicionei algumas constantes

--- app/src/main/java/ifsc/joe/domain/impl/Arqueiro.java
+++ app/src/main/java/ifsc/joe/domain/impl/Arqueiro.java
@@ -2,9 +2,12 @@ package ifsc.joe.domain.impl;
 
 import ifsc.joe.api.Coletador;
 import ifsc.joe.api.Guerreiro;
+import ifsc.joe.consts.Constantes;
 import ifsc.joe.enums.Recursos;
 
-import.java.util.Set;
+import java.util.Set;
+
+import static ifsc.joe.consts.Constantes.*;
 
 public class Arqueiro extends Personagem implements Coletador, Guerreiro {
 
@@ -13,4 +16,17 @@ public class Arqueiro extends Personagem implements Coletador, Guerreiro {
     static {
         COLETAVEIS = Set.of(Recursos.COMIDA, Recursos.OURO, Recursos.MADEIRA);
     }
+
+    private int flechas;
+    private int madeiraColetada;
+
+    public Arqueiro() {}
+      super(Constantes.ARQUEIRO_VIDA_INICIAL,
+            Constantes.ARQUEIRO_ATAQUE,
+            Constantes.ARQUEIRO_VELOCIDADE);
+            this.flechas = Constantes.ARQUEIRO_FLECHAS_INICIAL;
+            this.madeiraColetada = 0;
+}
+
+
 
