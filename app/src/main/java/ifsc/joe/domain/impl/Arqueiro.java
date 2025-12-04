12e9cb77969ebd612e2214f6b072fbcb5b181494
gelson-jr <gelson.j@aluno.ifsc.edu.br>
2025-12-04 19:32:32 -0300

    Adicionando constantes na classe arqueiro

--- app/src/main/java/ifsc/joe/domain/impl/Arqueiro.java
+++ app/src/main/java/ifsc/joe/domain/impl/Arqueiro.java
@@ -5,9 +5,10 @@ import ifsc.joe.api.Guerreiro;
 import ifsc.joe.consts.Constantes;
 import ifsc.joe.enums.Recursos;
 
+import java.util.Locale;
 import java.util.Set;
 
-import static ifsc.joe.consts.Constantes.*;
+
 
 public class Arqueiro extends Personagem implements Coletador, Guerreiro {
 
@@ -20,13 +21,45 @@ public class Arqueiro extends Personagem implements Coletador, Guerreiro {
     private int flechas;
     private int madeiraColetada;
 
-    public Arqueiro() {}
-      super(Constantes.ARQUEIRO_VIDA_INICIAL,
-            Constantes.ARQUEIRO_ATAQUE,
-            Constantes.ARQUEIRO_VELOCIDADE);
-            this.flechas = Constantes.ARQUEIRO_FLECHAS_INICIAL;
-            this.madeiraColetada = 0;
+    public Arqueiro() {
+        super(Constantes.ARQUEIRO_VIDA_INICIAL,
+                Constantes.ARQUEIRO_ATAQUE,
+                Constantes.ARQUEIRO_VELOCIDADE);
+        this.flechas = Constantes.ARQUEIRO_FLECHAS_INICIAL;
+        this.madeiraColetada = 0;
+    }
+
+    @Override
+    public boolean coletar(Recursos recursos) {
+        if (!COLETAVEIS.contains(recursos)) return false;
+        if (recursos == Recursos.MADEIRA) this.madeiraColetada++;
+        return true;
+    }
+
+    @Override
+    public String atacar(Personagem alvo) {
+        if (this.flechas-- <= 0) {
+            return "O arqueiro está sem flechas";
+        }
+        return String.format(
+                "O arqueiro ataca com %d de força.%n\tResultado: %s%n",
+                this.ataque,
+                alvo.sofrerDano(this.ataque)
+        );
+    }
+    public String produzirFlechas() {
+        if (this.madeiraColetada == 0) return "Arqueiro sem madeira para produção!";
+        this.madeiraColetada--;
+        return this.recarregarFlechas(Constantes.ARQUEIRO_FLECHAS_PRODUCAO);
+    }
+
+    public String recarregarFlechas(int quant) {
+        if (quant <= 0) return "Não é possível recarregar flechas negativas!";
+        this.flechas +=  quant;
+        return String.format("Arqueiro agora com %d flechas%n", quant);
+    }
 }
+
 
 
 
